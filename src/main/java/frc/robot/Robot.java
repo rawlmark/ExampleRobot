9064436d5cf8f16d53bffe35ccd3f6e9ba313900
FRC7891 <FRC.7891@gmail.com>
2019-10-05 12:25:16 -0600

    Added a comment.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -39,6 +39,7 @@ public class Robot extends TimedRobot {
     m_chooser.setDefaultOption("Default Auto", new ExampleCommand());
     // chooser.addOption("My Auto", new MyAutoCommand());
     SmartDashboard.putData("Auto mode", m_chooser);
+    //robot initialization goes here. YAY
   }
 
   /**
