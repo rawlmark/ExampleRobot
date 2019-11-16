ef852ecba1834d8c4517e3a7e5cca6c679d07b17
Watson <57297814+rawlmark@users.noreply.github.com>
2019-11-16 12:58:24 -0700

    201911161258

--- src/main/java/frc/robot/subsystems/DrivetrainSS.java
+++ src/main/java/frc/robot/subsystems/DrivetrainSS.java
@@ -7,7 +7,11 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.RobotMap;
 
 /**
  * Add your docs here.
@@ -15,11 +19,44 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class DrivetrainSS extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
+  public static TalonSRX leftMotor1; 
+  public static TalonSRX leftMotor2;
+  public static TalonSRX rightMotor1;
+  public static TalonSRX rightMotor2;
 
-  public void ArcadeDrive() {
-
+  public DrivetrainSS(){
+    //constructor
+    leftMotor1 = new TalonSRX(RobotMap.leftMotor1);
+    leftMotor2 = new TalonSRX(RobotMap.leftMotor2);
+    rightMotor1 = new TalonSRX(RobotMap.rightMotor1);
+    rightMotor2 = new TalonSRX(RobotMap.rightMotor2);
   }
 
+  public void ArcadeDrive() {
+    
+  }
+
+  public void halt(){
+    leftMotor1.set(ControlMode.PercentOutput, 0);
+    leftMotor2.set(ControlMode.PercentOutput, 0);
+    rightMotor1.set(ControlMode.PercentOutput, 0);
+    rightMotor2.set(ControlMode.PercentOutput, 0);
+  }
+  
+  public void move(){
+    leftMotor1.set(ControlMode.PercentOutput, 1);
+    leftMotor2.set(ControlMode.PercentOutput, 1);
+    rightMotor1.set(ControlMode.PercentOutput, 1);
+    rightMotor2.set(ControlMode.PercentOutput, 1);
+  }
+
+  public void set_speed(double leftspeed, double rightspeed){
+    leftMotor1.set(ControlMode.PercentOutput, leftspeed);
+    leftMotor2.set(ControlMode.PercentOutput, leftspeed);
+    rightMotor1.set(ControlMode.PercentOutput, rightspeed);
+    rightMotor2.set(ControlMode.PercentOutput, rightspeed);
+  }
+  
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
